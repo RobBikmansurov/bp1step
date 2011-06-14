a9b189b9b5d4350d2e45294a3c9d23a03aae68da
Rob Bikmansurov <robb@mail.ru>
2011-06-14 22:08:10 +0600

    3 test pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 BPDoc::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
