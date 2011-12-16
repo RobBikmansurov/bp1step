d807e4b962d0aa79a892746692da36829811110b
Rob Bikmansurov <robb@mail.ru>
2011-12-16 18:33:02 +0600

    сделан поискдокументов

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,12 @@
 class Document < ActiveRecord::Base
   belongs_to :bproce
+  def self.search(search)
+  if search
+    find(:all, :conditions => ['name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%"])
+  else
+    find(:all)
+  end
+end
+
+
 end
