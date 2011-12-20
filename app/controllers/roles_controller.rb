ef8aea932e2c41198d5790c7243df15271fd122a
Rob Bikmansurov <robb@mail.ru>
2011-12-20 15:41:46 +0600

    стили пейджинга, добавлен jquery - теперь работает удаление

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,82 +1,41 @@
 class RolesController < ApplicationController
-  # GET /roles
-  # GET /roles.json
+  respond_to :html, :xml, :json
+
   def index
     @roles = Role.search(params[:search], params[:page])
-    respond_to do |format|
-      format.html # index.html.erb
-##      format.json { render json: @roles }
-    end
+    respond_with($roles)
   end
-
-  # GET /roles/1
-  # GET /roles/1.json
-  def show
-    @role = Role.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @role }
-    end
-  end
-
-  # GET /roles/new
-  # GET /roles/new.json
+  
   def new
     @role = Role.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @role }
-    end
+    respond_with($role)
   end
 
-  # GET /roles/1/edit
+  def create
+    @role = Role.create(params[:role])
+    flash[:notice] = "Successfully created role." if $role.save
+    respond_with(@role)
+  end
+
+  def show
+    respond_with(@role = Role.find(params[:id]))
+  end
+
   def edit
     @role = Role.find(params[:id])
+    respond_with(@role)
   end
 
-  # POST /roles
-  # POST /roles.json
-  def create
-    @role = Role.new(params[:role])
-
-    respond_to do |format|
-      if @role.save
-        format.html { redirect_to @role, notice: 'Role was successfully created.' }
-        format.json { render json: @role, status: :created, location: @role }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @role.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /roles/1
-  # PUT /roles/1.json
   def update
     @role = Role.find(params[:id])
-
-    respond_to do |format|
-      if @role.update_attributes(params[:role])
-        format.html { redirect_to @role, notice: 'Role was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @role.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Successfully updated role."  if $role.update_attributes(params[:role])
+    respond_with(@role)
   end
 
-  # DELETE /roles/1
-  # DELETE /roles/1.json
   def destroy
     @role = Role.find(params[:id])
     @role.destroy
-
-    respond_to do |format|
-      format.html { redirect_to roles_url }
-      format.json { head :ok }
-    end
+    flash[:notice] = "Successfully destroyed role." if $role.save
+    respond_with(@role)
   end
 end
