01c81855e6c21a6e07a3c8622a1bb7874866d205
Rob Bikmansurov <robb@mail.ru>
2011-12-21 17:00:41 +0600

    косметика

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,7 @@ class Document < ActiveRecord::Base
   belongs_to :bproce
 
   def self.search(search, page)
-    paginate :per_page => 5, :page => page,
+    paginate :per_page => 10, :page => page,
            :conditions => ['name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%"],
            :order => 'part, name'
   end
