ed171ebffb5b29c2467ec3078d5734c7ceb0d98a
Rob Bikmansurov <robb@mail.ru>
2011-12-22 14:18:20 +0600

    Сортировка по столбцам Рабочие места

--- app/controllers/workplaces_controller.rb
+++ app/controllers/workplaces_controller.rb
@@ -1,83 +1,50 @@
 class WorkplacesController < ApplicationController
-  # GET /workplaces
-  # GET /workplaces.json
+  respond_to :html, :xml, :json
+  helper_method :sort_column, :sort_direction
+
   def index
-    @workplaces = Workplace.search(params[:search], params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @workplaces }
-    end
+    @workplaces = Workplace.search(params[:search]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
   end
 
-  # GET /workplaces/1
-  # GET /workplaces/1.json
   def show
-    @workplace = Workplace.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @workplace }
-    end
+    respond_with(@workplace = Workplace.find(params[:id]))
   end
 
-  # GET /workplaces/new
-  # GET /workplaces/new.json
   def new
     @workplace = Workplace.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @workplace }
-    end
+    respond_with(@workplace)
   end
 
-  # GET /workplaces/1/edit
   def edit
     @workplace = Workplace.find(params[:id])
   end
 
-  # POST /workplaces
-  # POST /workplaces.json
   def create
-    @workplace = Workplace.new(params[:workplace])
-
-    respond_to do |format|
-      if @workplace.save
-        format.html { redirect_to @workplace, notice: 'Workplace was successfully created.' }
-        format.json { render json: @workplace, status: :created, location: @workplace }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @workplace.errors, status: :unprocessable_entity }
-      end
-    end
+    @workplace = Workplace.create(params[:workplace])
+    flash[:notice] = "Successfully created workplace." if @workplace.save
+    respond_with(@workplace)
   end
 
-  # PUT /workplaces/1
-  # PUT /workplaces/1.json
   def update
     @workplace = Workplace.find(params[:id])
-
-    respond_to do |format|
-      if @workplace.update_attributes(params[:workplace])
-        format.html { redirect_to @workplace, notice: 'Workplace was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @workplace.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Successfully updated workplace."  if @workplace.update_attributes(params[:workplace])
+    respond_with(@workplace)
   end
 
-  # DELETE /workplaces/1
-  # DELETE /workplaces/1.json
   def destroy
     @workplace = Workplace.find(params[:id])
     @workplace.destroy
-
-    respond_to do |format|
-      format.html { redirect_to workplaces_url }
-      format.json { head :ok }
-    end
+    flash[:notice] = "Successfully destroyed workplace."  if @workplace.save
+    respond_with(@workplace)
   end
+
+private
+  def sort_column
+    params[:sort] || "designation"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
+  end
+
 end
--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -1,10 +1,12 @@
 class Workplace < ActiveRecord::Base
   validates :designation, :length => {:minimum => 8, :maximum => 50}
   has_many :bapps
-  def self.search(search, page)
-    paginate :per_page => 10, :page => page,
-           :conditions => ['designation LIKE ? or name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"],
-           :order => 'designation, name'
+  def self.search(search)
+   if search
+      where('designation LIKE ? or name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      scoped
+    end
   end
 
 end
