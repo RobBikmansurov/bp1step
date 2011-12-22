292968b97921237247867d2878fe593cc7445f4d
Rob Bikmansurov <robb@mail.ru>
2011-12-22 17:50:46 +0600

    Сортировка по столбцам Приложения

--- app/controllers/bapps_controller.rb
+++ app/controllers/bapps_controller.rb
@@ -1,83 +1,51 @@
 class BappsController < ApplicationController
-  # GET /bapps
-  # GET /bapps.json
+  respond_to :html, :xml, :json
+  helper_method :sort_column, :sort_direction
+
   def index
-    @bapps = Bapp.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @bapps }
-    end
+    @bapps = Bapp.search(params[:search]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
   end
-
-  # GET /bapps/1
-  # GET /bapps/1.json
-  def show
-    @bapp = Bapp.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @bapp }
-    end
-  end
-
-  # GET /bapps/new
-  # GET /bapps/new.json
+  
   def new
     @bapp = Bapp.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @bapp }
-    end
+    respond_with(@bapp)
   end
 
-  # GET /bapps/1/edit
+  def create
+    @bapp = Bapp.create(params[:bapp])
+    flash[:notice] = "Successfully created bapp." if @bapp.save
+    respond_with(@bapp)
+  end
+
+  def show
+    respond_with(@bapp = Bapp.find(params[:id]))
+  end
+
   def edit
     @bapp = Bapp.find(params[:id])
   end
 
-  # POST /bapps
-  # POST /bapps.json
-  def create
-    @bapp = Bapp.new(params[:bapp])
-
-    respond_to do |format|
-      if @bapp.save
-        format.html { redirect_to @bapp, notice: 'Bapp was successfully created.' }
-        format.json { render json: @bapp, status: :created, location: @bapp }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @bapp.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /bapps/1
-  # PUT /bapps/1.json
   def update
     @bapp = Bapp.find(params[:id])
-
-    respond_to do |format|
-      if @bapp.update_attributes(params[:bapp])
-        format.html { redirect_to @bapp, notice: 'Bapp was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @bapp.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Successfully updated bapp."  if @bapp.update_attributes(params[:bapp])
+    respond_with(@bapp)
   end
 
-  # DELETE /bapps/1
-  # DELETE /bapps/1.json
   def destroy
     @bapp = Bapp.find(params[:id])
     @bapp.destroy
-
-    respond_to do |format|
-      format.html { redirect_to bapps_url }
-      format.json { head :ok }
-    end
+    flash[:notice] = "Successfully destroyed bapp." if @bapp.save
+    respond_with(@bapp)
   end
+
+private
+  def sort_column
+    params[:sort] || "name"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
+  end
+
+
 end
