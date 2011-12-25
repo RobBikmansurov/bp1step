3295aa5a97ac491220326472a4fa7fe7cef2c468
Rob Bikmansurov <robb@mail.ru>
2011-12-25 18:55:18 +0600

    заголовки страниц + разборки с select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"  
     link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}  
   end  
+
+  def title(page_title)  
+    content_for(:title) { page_title }  
+  end  
+
 end  
