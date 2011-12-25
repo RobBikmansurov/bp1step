1c2216155fb9f24da973d41e4be16d8c37e23a6e
Rob Bikmansurov <robb@mail.ru>
2011-12-25 15:54:35 +0600

    руссификация сообщений об ошибках + категории

--- db/migrate/20111113142925_create_documents.rb
+++ db/migrate/20111113142925_create_documents.rb
@@ -5,6 +5,7 @@ class CreateDocuments < ActiveRecord::Migration
       t.string :filename
       t.string :description
       t.string :status
+      t.integer :status_id
       t.string :part
       t.date :approved
       t.string :place
