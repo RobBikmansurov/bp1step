9d8f6694676b5eec6b4931a52ab5e2758686739f
Rob Bikmansurov <robb@mail.ru>
2011-12-29 11:04:09 +0600

    игры с правильным отображением выбранного документа из списка

--- app/controllers/documentco_controller.rb
+++ app/controllers/documentco_controller.rb
@@ -1,2 +1,14 @@
 class DocumentcoController < ApplicationController
+  respond_to :html, :xml, :json
+  def index
+    @documents = Document.search(params[:search]).paginate(:per_page => 10, :page => params[:page])
+    @document = @documents.first
+  end
+
+  def show
+    @documents = Document.search(params[:search]).paginate(:per_page => 10, :page => params[:page])
+    @document = @documents.first
+    @document = @documents.find(params[:id])
+  end
+
 end
