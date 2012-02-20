4f5a896a306a380aacbcad0bdbdc842dac59e887
Rob Bikmansurov <robb@mail.ru>
2012-02-20 17:47:22 +0600

    мелкие эксперименты с документом

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -12,35 +12,23 @@ class DocumentsController < ApplicationController
 
   def update
     @document = Document.find(params[:id])
-
-    respond_to do |format|
-      if @document.update_attributes(params[:document])
-        format.html { redirect_to @document, notice: 'Document was successfully saved.' }
-      else
-        format.html { render action: "edit" }
-      end
-    end
+    flash[:notice] = "Successfully updated role."  if @document.update_attributes(params[:document])
+    respond_with(@document)
   end
 
   def show
-    @document = Document.find(params[:id])
+    respond_with(@document = Document.find(params[:id]))
   end
 
   def new
     @document = Document.new
+    respond_with(@document)
   end
 
   def create
-    @document = Document.new(params[:document])
-    respond_to do |format|
-      if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render json: @document, status: :created, location: @document }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
-    end
+    @document = Document.create(params[:document])
+    flash[:notice] = "Successfully created Document." if @document.save
+    respond_with(@document)
   end
 
   def destroy
