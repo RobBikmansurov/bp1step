6e89d13313c2777108040743b17d049570bd24aa
Rob Bikmansurov <robb@mail.ru>
2012-02-25 17:58:48 +0600

    роли в процессе

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,9 @@
 class Role < ActiveRecord::Base
   validates :name,  :presence => true, 
                     :length => {:minimum => 8, :maximum => 50}
+  belongs_to :bproce
   def self.search(search)
-   if search
+    if search
       where('name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%")
     else
       scoped
