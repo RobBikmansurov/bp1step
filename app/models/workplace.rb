37f5aafa366a621b51697cac30ebc8e2e98e3368
Rob Bikmansurov <robb@mail.ru>
2012-03-23 09:00:03 +0600

    исправлено: в Процессе показывается Designation, а не Name Рабочего места

--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -5,6 +5,7 @@ class Workplace < ActiveRecord::Base
   has_many :bproce_workplaces
   has_many :bproces, :through => :bproce_workplaces
   accepts_nested_attributes_for :bproce_workplaces, :allow_destroy => true 
+  accepts_nested_attributes_for :bproces
   #has_many :bapps
 
   def self.search(search)
