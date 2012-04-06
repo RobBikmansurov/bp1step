2aec12e5dc73974188b1444932658060e46f2c0b
Rob Bikmansurov <robb@mail.ru>
2012-04-06 18:30:51 +0600

    работает аутентификации в AD

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,28 +11,29 @@ class User < ActiveRecord::Base
   before_save :get_ldap_lastname, :get_ldap_firstname, :get_ldap_displayname, :get_ldap_email
 
   def get_ldap_lastname
-      Rails::logger.info("### Getting the users last name")
+      #Rails::logger.info("### Getting the users last name")
       tempname = Devise::LdapAdapter.get_ldap_param(self.username,"sn")
-      puts "\tLDAP returned lastname of " + tempname.encode('windows-1251','utf-8')
-      puts "\tLDAP returned lastname of " + tempname.encode('windows-1251')
+      tempname = tempname.force_encoding("UTF-8")
+      puts "\tLDAP returned lastname of " + tempname
       self.lastname = tempname
   end 
 
   def get_ldap_firstname
-      Rails::logger.info("### Getting the users first name")
+      #Rails::logger.info("### Getting the users first name")
       tempname = Devise::LdapAdapter.get_ldap_param(self.username,"givenname")
+      tempname = tempname.force_encoding("UTF-8")
       puts "\tLDAP returned firstname of " + tempname
       self.firstname = tempname
   end 
 
   def get_ldap_displayname
-      Rails::logger.info("### Getting the users display name")
+      #Rails::logger.info("### Getting the users display name")
       tempname = Devise::LdapAdapter.get_ldap_param(self.username,"displayname")
-      self.displayname = tempname
+      self.displayname = tempname.force_encoding("UTF-8")
   end 
 
   def get_ldap_email
-      Rails::logger.info("### Getting the users email address")
+      #Rails::logger.info("### Getting the users email address")
       tempmail = Devise::LdapAdapter.get_ldap_param(self.username,"mail")
       self.email = tempmail
   end
