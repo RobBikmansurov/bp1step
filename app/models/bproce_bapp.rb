abee2a84c9d5385f283fa1dd13a0422de3a96319
Rob Bikmansurov <robb@mail.ru>
2012-06-01 18:01:52 +0600

    мелкие правки и хотелки

--- app/models/bproce_bapp.rb
+++ app/models/bproce_bapp.rb
@@ -1,6 +1,7 @@
 class BproceBapp < ActiveRecord::Base
   validates :bproce_id, :presence => true
   validates :bapp_id, :presence => true
+  validates :apurpose, :presence => true
   belongs_to :bproce
   belongs_to :bapp
 end
