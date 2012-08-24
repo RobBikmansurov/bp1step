8a6741a24aa2a94f63a88ab6f7d83160216d0a57
Rob Bikmansurov <robb@mail.ru>
2012-08-24 17:22:05 +0600

    добавил изменения в git

---
+++ db/migrate/20120823102543_add3_fields_to_users.rb
@@ -0,0 +1,15 @@
+class Add3FieldsToUsers < ActiveRecord::Migration
+  def up
+    add_column :users, :department, :string
+    add_column :users, :position, :string
+    add_column :users, :office, :string
+    add_column :users, :phone, :string
+  end
+
+  def down
+    remove_column :users, :department
+    remove_column :users, :position
+    remove_column :users, :office
+    remove_column :users, :phone
+  end
+end
