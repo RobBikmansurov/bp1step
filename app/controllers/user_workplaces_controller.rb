9e93e2156a23816f8eb2bf8d2550f100a6bb5b24
Rob Bikmansurov <robb@mail.ru>
2012-08-28 17:44:31 +0600

    отладка добавления рабочего места пользователю

--- app/controllers/user_workplaces_controller.rb
+++ app/controllers/user_workplaces_controller.rb
@@ -5,4 +5,15 @@ class UserWorkplacesController < ApplicationController
   	@uw = User.find(params[:id])
   	respond_with(@workplaces = @uw.workplaces)
   end
+
+  def new
+    @user_workplace = UserWorkplace.new
+  end
+
+  def create
+    @bproce_bapp = BproceBapp.create(params[:bproce_bapp])
+    flash[:notice] = "Successfully created bproce_bapp." if @bproce_bapp.save
+    respond_with(@bproce_bapp.bapp)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ BPDoc::Application.routes.draw do
   resources :bproce_workplaces, :only => [:new, :create, :destroy, :show]
   resources :bproce_documents, :only => [:show]
   resources :bproce_roles, :only => [:show]
+  resources :user_workplaces, :only => [:new, :create, :destroy]
   
   match '/about' => 'pages#about', :via => :get
   get 'pages/about'
