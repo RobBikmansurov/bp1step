9d7baa358d381808aca838287ee7fb567d6695f3
Rob Bikmansurov <robb@mail.ru>
2012-09-01 21:15:33 +0600

    добавлены роли доступа, введены ограничения функций удалить/добавить для всех списков

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,26 +1,4 @@
 class Role < ActiveRecord::Base
-  validates :name, :presence => true,
-                   :length => {:minimum => 5, :maximum => 50}
-  validates :description, :presence => true,
-                          :length => {:minimum => 8}
-  validates :bproce_id, :presence => true
-
-  # роль участвует в процессе
-  belongs_to :bproce
-  # роль может исполняться многими пользователями
-  has_many :user_role  # пользователь может иметь много ролей
-  has_many :users, :through => :user_role
-
-  # роль может исполняется на нескольких рабочих местах
-  #has_and_belongs_to_many :workplaces
-
-
-  def self.search(search)
-    if search
-      where('name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%")
-    else
-      scoped
-    end
-  end
-
+  has_many :user_roles
+  has_many :user, :through => :user_roles
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,33 +1,5 @@
 require 'spec_helper'
 
 describe Role do
-  before(:each) do
-    @role = Role.new
-    @role.bproce_id = 1
-    @role.name = 'test_role_name'
-    @role.description = "test_role_description"
-  end
-
-  it "should be valid" do
-    @role.should be_valid
-  end
-
-  it "should require name" do
-    @role.name = nil
-    @role.should_not be_valid
-  end
-  it "should require uniqueness name" do
-    @role1 = Role.new
-    @role1.name = "test_workplaces_1"
-    @role1.should_not be_valid
-  end
-  it "should require long name" do
-    @role.name = "1234"
-    @role.should_not be_valid
-  end
-  it "should require max lenght name 50" do
-    @role.name = "maxlenght1maxlenght2maxlenght3maxlenght4maxlenght5_"
-    @role.should_not be_valid
-  end
-
+  pending "add some examples to (or delete) #{__FILE__}"
 end
