25991ddd9c050ce359c9b8cbd77598190562533a
Rob Bikmansurov <robb@mail.ru>
2012-10-01 22:14:18 +0600

    тест

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ BPDoc::Application.routes.draw do
   resources :user_workplaces
   resources :user_business_roles, :only => [:new, :create, :destroy]
   resources :bproces
-  resources :bprocess do
+  resources :bproces do
     resources :bapps
   end
   match '/bproceses' => 'bproces#list', :via => :get
