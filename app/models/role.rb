b17758c996a9ab451eb31d3529bc691b7fc8e92a
Rob Bikmansurov <robb@mail.ru>
2012-11-02 11:47:29 +0600

    добавлены валидации для Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,7 @@
 class Role < ActiveRecord::Base
+  validates :description, :presence => true
+  validates :name, :presence => true, :length => {:minimum => 5}
+
   has_many :user_roles
   has_many :user, :through => :user_roles
 end
