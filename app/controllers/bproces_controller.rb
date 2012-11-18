f33cc09cb94ae9fdd91bc47d8d8e2dac5e6d9a59
Rob Bikmansurov <robb@mail.ru>
2012-11-18 19:04:29 +0600

    создан отчет - карточка процесса, в остальных отчетах разобрался с переменными

--- app/controllers/bproces_controller.rb
+++ app/controllers/bproces_controller.rb
@@ -2,7 +2,7 @@ class BprocesController < ApplicationController
   respond_to :html
   respond_to :pdf, :xml, :json, :only => :index
   helper_method :sort_column, :sort_direction
-  before_filter :get_bproce, :except => :card
+  before_filter :get_bproce, :except => :index
 
   def list
     @bproces = Bproce.search(params[:search]).order(sort_column + ' ' + sort_direction)
@@ -25,7 +25,6 @@ class BprocesController < ApplicationController
   end
 
   def card
-    @bproces = Bproce.order(sort_column + ' ' + sort_direction)
     respond_to do |format|
       format.html { print_card }
       format.pdf { print_card }
@@ -89,11 +88,20 @@ private
 
   def print
     report = ODFReport::Report.new("reports/bprocess.odt") do |r|
-      r.add_field "REPORT_DATE", Date.today
+      nn = 0 # порядковый номер строки
       r.add_table("TABLE_01", @bproces, :header=>true) do |t|
-        t.add_column(:name, :name)
-        t.add_column(:fullname, :fullname)
-        t.add_column(:goal, :goal)
+        t.add_column(:nn) do |ca| # порядковый номер строки таблицы
+          nn += 1
+          "#{nn}."
+        end
+        t.add_column(:owner) do |bproce|  # владелец процесса, если задан
+          if bproce.user_id
+            bproce.user.displayname
+          end
+        end
+        t.add_column(:name)
+        t.add_column(:fullname)
+        t.add_column(:goal)
       end
       r.add_field "USER_POSITION", current_user.position
       r.add_field "USER_NAME", current_user.displayname
@@ -108,16 +116,82 @@ private
   def print_card
     report = ODFReport::Report.new("reports/bp-card.odt") do |r|
       r.add_field "REPORT_DATE", Date.today
-      @nn = 0
-      r.add_table("TABLE_01", @bproces, :header=>true) do |t|
-        # TODO: здесь надо вставить порядковый номер строки в таблице
-        # t.add_column(:nn, :id)
-        t.add_column(:name)
-        t.add_column(:fullname)
-        t.add_column(:goal)
+      r.add_field :shortname, @bproce.shortname
+      r.add_field :name, @bproce.name
+      r.add_field :fullname, @bproce.fullname
+      r.add_field :goal, @bproce.goal
+      if @bproce.parent_id
+        r.add_field :parent, @bproce.parent.name
+      else
+        r.add_field :parent, "-"
       end
+      if @bproce.user_id  # владелец процесса
+        r.add_field :owner, @bproce.user.displayname
+      else
+        r.add_field :owner, "-"
+      end
+
+      nn = 0 # порядковый номер строки для документов
+      r.add_table("TABLE_DOCS", @bproce.documents, :header=>true, :skip_if_empty => true) do |t|
+        if @bproce.documents.count > 0  # если документов нет - пустая таблица не будет выведена
+          t.add_column(:nn) do |ca| # порядковый номер строки таблицы
+            nn += 1
+          end
+          t.add_column(:nd) do |document|
+            ndoc = document.name
+          end
+          t.add_column(:approved)
+          t.add_column(:responsible) do |document|  # владелец документа, если задан
+            if document.responsible
+              u=User.find(document.responsible)
+              "#{u.displayname}"
+            end
+          end
+        end
+      end
+
+      rr = 0 # порядковый номер строки для ролей
+      @roles = @bproce.business_roles
+      r.add_table("TABLE_ROLES", @roles, :header=>true, :skip_if_empty => true) do |t|
+        if @roles.count > 0  # если ролей нет - пустая таблица не будет выведена
+          t.add_column(:rr) do |nn| # порядковый номер строки таблицы
+            rr += 1
+          end
+          t.add_column(:nr, :name)
+          t.add_column(:description)
+        end
+      end
+
+      ww = 0 # порядковый номер строки для рабочих мест
+      @workplaces = @bproce.workplaces
+      r.add_table("TABLE_PLACES", @workplaces, :header=>true, :skip_if_empty => true) do |t|
+        if @workplaces.count > 0  # если рабочих мест нет - пустая таблица не будет выведена
+          t.add_column(:ww) do |nn| # порядковый номер строки таблицы
+            ww += 1
+          end
+          t.add_column(:nw, :name)
+          t.add_column(:designation)
+          t.add_column(:loca, :location)
+          t.add_column(:description)
+        end
+      end
+
+      pp = 0 # порядковый номер строки для приложений
+      @bapps = @bproce.bapps
+      r.add_table("TABLE_BAPPS", @bapps, :header=>true, :skip_if_empty => true) do |t|
+        if @bapps.count > 0  # если приложений нет - пустая таблица не будет выведена
+          t.add_column(:pp) do |nn| # порядковый номер строки таблицы
+            pp += 1
+          end
+          t.add_column(:na, :name)
+          t.add_column(:description)
+          t.add_column(:purpose)
+        end
+      end
+
       r.add_field "USER_POSITION", current_user.position
       r.add_field "USER_NAME", current_user.displayname
+      r.add_field "REPORT_DATE", Date.today
     end
     report_file_name = report.generate
     send_file(report_file_name,
