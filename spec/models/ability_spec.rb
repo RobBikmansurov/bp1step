c122d5844f853f1c86dca9b960ffd991b4d89176
Rob Bikmansurov <robb@mail.ru>
2012-11-27 17:59:12 +0600

    реализованы и протестированы права незарегистрированного пользователя

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,52 @@
+require "cancan/matchers"
+require "spec_helper"
+
+describe Ability do
+  let(:guest) { Ability.new(user) }
+  let(:any_document) { Document.new() }
+  let(:paid_article) { Bapp.new() }
+
+  context "guest user" do   # незарегистрированный пользователь
+    let(:user) { nil }
+
+    it "can :read any Objects" do
+      guest.can?(:read, Bapp).should be_true
+      guest.can?(:read, Bproce).should be_true
+      guest.can?(:read, BproceBapp).should be_true
+      guest.can?(:read, BproceWorkplace).should be_true
+      guest.can?(:read, BusinessRole).should be_true
+      guest.can?(:read, Directive).should be_true
+      guest.can?(:read, Document).should be_true
+      guest.can?(:read, Role).should be_true
+      guest.can?(:read, User).should be_true
+      guest.can?(:read, UserBusinessRole).should be_true
+      guest.can?(:read, UserWorkplace).should be_true
+      guest.can?(:read, Workplace).should be_true
+    end
+    it "cannot :manage any Objects" do
+      guest.can?(:manage, Bapp).should be_false
+      guest.can?(:manage, Bproce).should be_false
+      guest.can?(:manage, BproceBapp).should be_false
+      guest.can?(:manage, BproceWorkplace).should be_false
+      guest.can?(:manage, BusinessRole).should be_false
+      guest.can?(:manage, Directive).should be_false
+      guest.can?(:manage, Document).should be_false
+      guest.can?(:manage, Role).should be_false
+      guest.can?(:manage, User).should be_false
+      guest.can?(:manage, UserBusinessRole).should be_false
+      guest.can?(:manage, UserWorkplace).should be_false
+      guest.can?(:manage, Workplace).should be_false
+    end
+    it "cannot :show User" do
+      guest.can?(:show, User).should be_false # не видит подробностей о пользователе
+      guest.can?(:index, User).should be_true
+    end
+    it "can't assign roles" do
+      guest.can?(:assign_roles, User).should be_false
+    end
+    it "can't view document" do
+      guest.can?(:view_document, Document).should be_false
+    end
+  end
+
+end
