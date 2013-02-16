6912e3bab00ffd0d21dee1714217d09504becf52
Rob Bikmansurov <robb@mail.ru>
2013-02-16 21:13:28 +0600

    добавлен формат для отобажения даты и времени с помощью .to_s(:ru_datetime)

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,4 @@ require File.expand_path('../application', __FILE__)
 BPDoc::Application.initialize!
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
+Time::DATE_FORMATS[:ru_datetime] = "%d.%m.%Y %H:%M:%S"
