f334e1120a0481457e45c78e002f5a5930e2edcf
Rob Bikmansurov <robb@mail.ru>
2013-02-25 17:53:21 +0600

    разборки с autocomplete

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,6 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.jQuery ->
 jQuery ->
-  $('#document_owner_name').autocomplete
-    source: ['foo', 'forte', 'fif']
-    source: $('#document_owner_name').data('autocomplete-source')
+  $("#document_owner_name").autocomplete
+    source: ["foo', 'forte', 'fif"]
+//    source: $('#document_owner_name').data('autocomplete-source')
