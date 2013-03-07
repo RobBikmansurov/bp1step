026fa89e559d351d47893a02b265e6c2052a10f0
Rob Bikmansurov <robb@mail.ru>
2013-03-07 22:26:08 +0600

    протоколирование действий при назначении пользователя на роль и на рабочее место

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,4 +1,7 @@
 class UserRole < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
   belongs_to :role
   belongs_to :user
 end
