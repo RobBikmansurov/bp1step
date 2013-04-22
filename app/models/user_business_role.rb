71911294c59c8afda2ab5aa27288fb1bdf6295b0
Rob Bikmansurov <robb@mail.ru>
2013-04-22 16:01:48 +0600

    в форме процесса добавлен выбор приложений

--- app/models/user_business_role.rb
+++ app/models/user_business_role.rb
@@ -4,6 +4,7 @@ class UserBusinessRole < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :business_role_id, :presence => true
+
   belongs_to :user
   belongs_to :business_role
 
