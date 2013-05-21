959841c885ed6d52dac94b221496a9ebf7587225
Rob Bikmansurov <robb@mail.ru>
2013-05-21 16:26:48 +0600

    fix: исправлена ошибка с именем файла документа

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,4 +55,6 @@ BPDoc::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  Paperclip.options[:command_path] = "/usr/bin/"
+
 end
