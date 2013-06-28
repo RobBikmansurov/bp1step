1a00f3ff340fcec201d0fde837384b0908797e90
Rob Bikmansurov <robb@mail.ru>
2013-06-28 11:45:48 +0600

    тесты запросов проходят

--- spec/requests/bproce_documents_spec.rb
+++ spec/requests/bproce_documents_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
-describe "BproceWorkplaces" do
+describe "BproceDocuments" do
   before(:each) do
     @bp = create(:bproce)
   end
-  describe "GET /bproce_workplaces/bproce" do
+
+  describe "GET /bproce_document/1" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get bproce_workplace_path(@bp)
+      get bproce_document_path(@bp)
       response.status.should be(200)
     end
   end
