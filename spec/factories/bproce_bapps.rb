6a5fbae053af5bed647a194a39122e1f5750c878
Rob Bikmansurov <robb@mail.ru>
2013-06-28 11:01:53 +0600

    борьба с тестами

--- spec/factories/bproce_bapps.rb
+++ spec/factories/bproce_bapps.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :bproce_bapp do
-    bproce_id  1
-    bapp_id  1
-    apurpose 'purpose_1'
+    apurpose 'bapps purpose in process'
+    bproce
+    bapp
   end
 end
