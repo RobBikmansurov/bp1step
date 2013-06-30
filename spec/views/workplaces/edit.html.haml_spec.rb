a42081f943ff4adebc2b2a6edd0dd3f10d99e62a
Rob Bikmansurov <robb@mail.ru>
2013-07-01 00:05:37 +0600

    отладка тестов view рабочиз мест

--- spec/views/workplaces/edit.html.haml_spec.rb
+++ spec/views/workplaces/edit.html.haml_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-describe "workplaces/new.html.erb" do
+describe "workplaces/edit.html.haml" do
   before(:each) do
-    assign(:workplace, stub_model(Workplace,
+    @workplace = assign(:workplace, stub_model(Workplace,
       :designation => "MyString",
       :name => "MyString",
       :description => "MyString",
       :typical => false,
       :location => "MyString"
-    ).as_new_record)
+    ))
   end
 
-  it "renders new workplace form" do
+  it "renders the edit workplace form" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => workplaces_path, :method => "post" do
+    assert_select "form", :action => workplaces_path(@workplace), :method => "post" do
       assert_select "input#workplace_designation", :name => "workplace[designation]"
       assert_select "input#workplace_name", :name => "workplace[name]"
       assert_select "input#workplace_description", :name => "workplace[description]"
