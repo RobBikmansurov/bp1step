673a7ebb83833facbaf9594c22da266393611b48
Rob Bikmansurov <robb@mail.ru>
2013-07-03 14:29:05 +0600

    удален users_controller_spec - не могу решить проблему с тестированием пользователей в LDAP

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,14 +5,7 @@ FactoryGirl.define do
     email         { "#{username}@example.com" }
     password              "foobar"
     password_confirmation "foobar"
-  end
-  
-  factory :auth_user do
-    sequence(:username) { |n| "name#{n}" }
-    password "foobar"
-    email { "#{username}@example.com" }
-    password_confirmation "foobar"
-    roles_ids [1]
+    encrypted_password 'secret'
   end
 
 end
