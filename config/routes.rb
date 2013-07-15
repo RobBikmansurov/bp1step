73806a086223b9cf8c769ad62817d465a028f2df
Rob Bikmansurov <robb@mail.ru>
2013-07-15 12:30:42 +0600

    добавлено формирование распоряжения на роли в процессе

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,12 @@ BPDoc::Application.routes.draw do
     collection do
       get :manage
       post :rebuild
+      get :list
+    end
+    member do
+      get :order  # распоряжение о назначении исполнителей в процесс
+      get :doc    # описание процесса
+      get :card   # карточка процесса
     end
   end
   get 'bproces/tags/:tag', to: 'bproces#index', as: :tag
@@ -44,10 +50,10 @@ BPDoc::Application.routes.draw do
   resources :user_workplaces
   resources :user_business_roles, :only => [:new, :create, :destroy]
 
-  match '/bproceses' => 'bproces#list', :via => :get  # получение полного списка процессов
+  #match '/bproceses' => 'bproces#list', :via => :get  # получение полного списка процессов
   match '/bprocess' => 'bproces#manage', :via => :get  # получение полного списка процессов
-  match '/bproces/:id/card' => 'bproces#card', :via => :get  # карточка процесса
-  match '/bproces/:id/doc' => 'bproces#doc', :via => :get  # заготовка описания процесса
+  #match '/bproces/:id/card' => 'bproces#card', :via => :get  # карточка процесса
+  #match '/bproces/:id/doc' => 'bproces#doc', :via => :get  # заготовка описания процесса
   match '/workplaces/switch' => 'workplaces#switch', :via => :get  # подключения рабочих мест
   resources :workplaces
   
