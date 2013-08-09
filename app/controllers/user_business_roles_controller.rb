21204cd849c0d9e420697f71cc7d385fca16840f
Rob Bikmansurov <robb@mail.ru>
2013-08-09 17:20:38 +0600

    в email-оповещения при изменении исполнителей ролей добавлено ФИО того, кто это сделал

--- app/controllers/user_business_roles_controller.rb
+++ app/controllers/user_business_roles_controller.rb
@@ -16,14 +16,14 @@ class UserBusinessRolesController < ApplicationController
     @user_business_role = UserBusinessRole.create(params[:user_business_role])
     @business_role = BusinessRole.find(@user_business_role.business_role_id)
     flash[:notice] = "Successfully created user_business_role." if @user_business_role.save
-    UserBusinessRoleMailer.user_create_role(@user_business_role).deliver    # оповестим нового исполнителя
+    UserBusinessRoleMailer.user_create_role(@user_business_role, current_user).deliver    # оповестим нового исполнителя
     respond_with(@business_role)
   end
 
   def destroy
     @user_business_role = UserBusinessRole.find(params[:id])
     @business_role = BusinessRole.find(@user_business_role.business_role_id)
-    UserBusinessRoleMailer.user_delete_role(@user_business_role).deliver    # оповестим бывшего исполнителя
+    UserBusinessRoleMailer.user_delete_role(@user_business_role, current_user).deliver    # оповестим бывшего исполнителя
     @user_business_role.destroy
     respond_with(@business_role)
   end
