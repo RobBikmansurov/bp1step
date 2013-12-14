40ddf1f3dfabad2199b3e50f2b6ecf2c2b4e5fe6
Rob Bikmansurov <robb@mail.ru>
2013-12-14 15:55:38 +0600

    перенос БД sqlite3 -> postgresql

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,7 +7,7 @@ class Role < ActiveRecord::Base
   has_many :user_roles
   has_many :users, :through => :user_roles
 
-  attr_accessible :name, :description
+  attr_accessible :name, :description, :note
 
   def self.search(search)
     if search
