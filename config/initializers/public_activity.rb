8894f072a473d5654b1947bfeb1b294dfb06ffa5
Rob Bikmansurov <robb@mail.ru>
2013-12-14 15:55:38 +0600

    перенос БД sqlite3 -> postgresql

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -1,4 +1,4 @@
-# хак для исключения ошибки  -Can't mass-assign protected attributes for PublicActivity::Activity: key, parameters, owner, recipient
+# для исключения ошибки - Can't mass-assign protected attributes for PublicActivity::Activity: key, parameters, owner, recipient
 
 PublicActivity::Activity.class_eval do
     attr_accessible :key, :owner, :parameters, :recipient, :trackable
