3a886ea1f21fc2a9355f77be14499b38811c842d
Rob Bikmansurov <robb@mail.ru>
2013-12-16 15:38:28 +0600

    fixed untracked files

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-BPDoc::Application.initialize!
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-Time::DATE_FORMATS[:ru_datetime] = "%d.%m.%Y %H:%M:%S"
