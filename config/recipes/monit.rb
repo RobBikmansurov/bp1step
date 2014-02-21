55bc48e0510eeb8bdb3ca9ff18bc163358bfdaec
Rob Bikmansurov <robb@mail.ru>
2014-02-21 14:05:42 +0600

    в monit добавлен контроль postgresql

--- config/recipes/monit.rb
+++ config/recipes/monit.rb
@@ -10,7 +10,7 @@ namespace :monit do
     monit_config "monitrc", "/etc/monit/monitrc"
     nginx
     files
-    #postgresql
+    postgresql
     unicorn
     syntax
     reload
@@ -20,7 +20,7 @@ namespace :monit do
   task(:nginx, roles: :web) { monit_config "nginx" }
   task(:files, roles: :web) { monit_config "files" }
   task(:store, roles: :web) { monit_config "store" }
-  #task(:postgresql, roles: :db) { monit_config "postgresql" }
+  task(:postgresql, roles: :db) { monit_config "postgresql" }
   task(:unicorn, roles: :app) { monit_config "unicorn" }
 
   %w[start stop restart syntax reload].each do |command|
