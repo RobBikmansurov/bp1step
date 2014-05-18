b313c49cc113f9a5274956d1026c6a62bf628af1
Rob Bikmansurov <robb@mail.ru>
2014-05-18 21:27:18 +0600

    add: тесты моделей Договор

--- spec/models/bproce_contract_spec.rb
+++ spec/models/bproce_contract_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe BproceContract do
-  pending "add some examples to (or delete) #{__FILE__}"
+  PublicActivity.enabled = false
+
+  context "associations" do
+    it { should belong_to(:bproce) }
+    it { should belong_to(:contract) }
+  end
+
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
+PublicActivity.enabled = false
+
 describe Contract do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validates" do
+    it { should validate_presence_of(:number) }
+    it { should ensure_length_of(:number).is_at_least(1).is_at_most(20) }
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_least(3).is_at_most(50) }
+    it { should validate_presence_of(:description) }
+    it { should ensure_length_of(:description).is_at_least(8).is_at_most(255) }
+    it { should validate_presence_of(:status) }
+    it { should ensure_length_of(:status).is_at_least(5).is_at_most(15) }
+  end
+
+  context "associations" do
+    it { should belong_to(:owner) }
+    it { should have_many(:bproce_contract).dependent(:destroy) }
+    it { should belong_to(:owner).class_name(:User) }
+  end
 end
