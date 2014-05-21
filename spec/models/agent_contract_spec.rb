ea377325681f2c75c2cd0e11b87408fab59c1977
Rob Bikmansurov <robb@mail.ru>
2014-05-21 17:37:39 +0600

    fix: отображение контрагентов

--- spec/models/agent_contract_spec.rb
+++ spec/models/agent_contract_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
+PublicActivity.enabled = false
+
 describe AgentContract do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validates" do
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_least(3).is_at_most(255) }
+  end
+
+  context "associations" do
+    it { should have_many(:agent_contract).dependent(:destroy) }
+    it { should belong_to(:contract).class_name(:agent_contract) }
+  end
 end
--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Agent do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validates" do
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_least(3).is_at_most(255) }
+  end
+
+  context "associations" do
+    it { should have_many(:agent_contract).dependent(:destroy) }
+  end
 end
