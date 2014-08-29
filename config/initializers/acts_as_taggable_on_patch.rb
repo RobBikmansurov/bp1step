24dc13affe86a4d4ea6dd20bbac17f6fdc08a8d1
Rob Bikmansurov <robb@mail.ru>
2014-08-29 12:30:37 +0600

    исправлена ошибка WARNING: Can't mass-assign protected attributes for ActsAsTaggableOn

---
+++ config/initializers/acts_as_taggable_on_patch.rb
@@ -0,0 +1,11 @@
+#https://github.com/mbleigh/acts-as-taggable-on/issues/389
+# fix error "WARNING: Can't mass-assign protected attributes for ActsAsTaggableOn"
+module ActsAsTaggableOn
+  class Tag
+    attr_accessible :name
+  end
+
+  class Tagging
+    attr_accessible :tag_id, :context, :taggable
+  end
+end
