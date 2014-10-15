0c173fc9f4e873df0ebc09734cedd814f8718423
Rob Bikmansurov <robb@mail.ru>
2014-10-15 10:28:41 +0600

    add: из Show можно увидеть протоколы событий по изменению конкретного Процесса. Документа, БизнесРоли

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,12 @@
 class ActivitiesController < ApplicationController
   def index
-  	@activities = PublicActivity::Activity.order("created_at desc").paginate(:per_page => 100, :page => params[:page])
+    if params[:type].present?
+      @activities = PublicActivity::Activity.where(trackable_type: "#{params[:type]}").paginate(:per_page => 100, :page => params[:page])
+      if params[:id].present?
+        @activities = PublicActivity::Activity.where(trackable_type: "#{params[:type]}", trackable_id: "#{params[:id]}").paginate(:per_page => 100, :page => params[:page])
+      end
+    else
+      @activities = PublicActivity::Activity.order("created_at desc").paginate(:per_page => 100, :page => params[:page])
+    end
   end
 end
