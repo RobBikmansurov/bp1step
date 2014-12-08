924d1e0c41f492bc051de304874be51a8240ff19
Rob Bikmansurov <robb@mail.ru>
2014-12-08 11:31:23 +0500

    fix: проходит фейковый тест контроллера Home

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,17 +1,29 @@
-require 'spec_helper'
-
-describe HomeController do
+RSpec.describe HomeController, type: :controller do
 
   before(:each) do
-    @user = FactoryGirl.create(:user)
-    @user.roles << Role.find_or_create_by(name: 'admin', description: 'description')
-    sign_in @user
+    #@user = FactoryGirl.create(:user)
+    #@user.roles << Role.find_or_create_by(name: 'admin', description: 'description')
+    #sign_in @user
+
+    #allow(controller).to receive(:authenticate_user!).and_return(true)
+    #allow(controller).to receive(:signed_in?).and_return(false)
+
+    #allow(controller).to receive(:signed_in?).and_return(true)
+    #allow(controller).to receive(:signed_in?).and_return(false)
+
   end
   
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
-      response.should be_success
+      allow(controller).to receive(:signed_in?).and_return(false) # unsigned user
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(:success)
+
+      allow(controller).to receive(:signed_in?).and_return(true) # signed user
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(:success)
     end
   end
 
