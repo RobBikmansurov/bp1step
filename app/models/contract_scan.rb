d70e0fac12c8ad0fe99f71f237578bf1c06080be
Rob Bikmansurov <robb@mail.ru>
2014-12-18 12:54:16 +0500

    fix: добавлены графические файлы jpeg, gif, tiff

--- app/models/contract_scan.rb
+++ app/models/contract_scan.rb
@@ -4,7 +4,7 @@ class ContractScan < ActiveRecord::Base
 
   default_scope { order(:name) }
 
-  validates :contract_id, presence: true   # должен иметь ответственного
+  validates :contract_id, presence: true   # относится к контракту
   validates :name, :presence => true,
                    :length => {:minimum => 3, :maximum => 255}
   
@@ -18,11 +18,12 @@ class ContractScan < ActiveRecord::Base
   validates :scan, :attachment_presence => true
   do_not_validate_attachment_file_type :scan  #paperclip >4.0
   validates_attachment_content_type :scan, 
-                                    :content_type => ['application/pdf', 'applications/vnd.pdf', 'binary/octet-stream',
-                                                      'application/vnd.oasis.opendocument.text', 'application/vnd.oasis.opendocument.spreadsheet',
-                                                      'application/vnd.ms-excel', 'application/msword',
-                                                      'application/doc', 'application/rtf',
-                                                      'application/octet-stream', 'application/force-download']
+  :content_type => ['application/pdf', 'applications/vnd.pdf', 'binary/octet-stream',
+                    'image/jpeg', 'image/gif', 'image/tiff',
+                    'application/vnd.oasis.opendocument.text', 'application/vnd.oasis.opendocument.spreadsheet',
+                    'application/vnd.ms-excel', 'application/msword',
+                    'application/doc', 'application/rtf',
+                    'application/octet-stream', 'application/force-download']
   attr_accessible :contract_id, :name, :scan
 
 end
