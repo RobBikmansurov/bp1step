35b8e38d91da1122df13e10bb809f9ca0169c590
Rob Bikmansurov <robb@mail.ru>
2015-02-05 11:16:11 +0500

    add: добавлен отчет по договорам

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -16,12 +16,20 @@ class ContractsController < ApplicationController
   def index
     if params[:status].present? #  список договоров, имеющих конкретный статус
       @contracts = Contract.where(:status => params[:status]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
+      @title_doc = 'статус [' + params[:status] + ']'
     else
       if params[:type].present? #  список договоров, имеющих конкретный тип
         @contracts = Contract.where(:contract_type => params[:type]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
+        @title_doc = 'тип [' + params[:type] + ']'
       else
         if params[:place].present? #  список договоров, хранящихся в конкретном месте
-          @contracts = Contract.where(:contract_place => params[:place]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
+          if params[:place].size == 0
+            @contracts = Contract.where("contract_place = ''").order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
+            @title_doc = 'место хранения оригинала [не указано]'
+          else
+            @contracts = Contract.where(:contract_place => params[:place]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
+            @title_doc = 'место хранения оригинала [' + params[:place] + ']'
+          end
         else
           if sort_column == 'lft'
             @contracts = Contract.search(params[:search]).order(:lft).paginate(:per_page => 10, :page => params[:page])
@@ -31,7 +39,11 @@ class ContractsController < ApplicationController
         end
       end
     end
-  end
+    respond_to do |format|
+      format.html
+      format.odt { print }
+      #format.pdf { print }
+    end  end
 
   def show
     @subcontracts = Contract.where("lft>? and rgt<?", @contract.lft, @contract.rgt).order("lft")
@@ -133,6 +145,45 @@ class ContractsController < ApplicationController
   end
 
   private
+
+  def print
+    report = ODFReport::Report.new("reports/contracts.odt") do |r|
+      nn = 0  # порядковый номер документа
+      nnp = 0
+      first_part = 0  # номер раздела для сброса номера документа в разделе
+      r.add_field "REPORT_DATE", Date.today.strftime('%d.%m.%Y')
+      @title_doc = '' if !@title_doc
+      if params[:page].present?
+        @title_doc = @title_doc + '  стр.' + params[:page]
+      end
+      r.add_field "REPORT_TITLE", @title_doc
+      r.add_table("TABLE_01", @contracts, :header => true) do |t|
+        t.add_column(:nn) do |ca|
+          nn += 1
+          "#{nn}."
+        end
+        t.add_column(:name) do |contract|
+          if contract.date_begin
+            d = ' от ' + contract.date_begin.strftime('%d.%m.%Y')
+          else
+            d = ' без даты'
+          end
+          s = contract.contract_type + ' №' + contract.number + ' ' + contract.name + d + ' ' + contract.status
+          "#{s}"
+        end
+        t.add_column(:agent, :agent_name)
+        t.add_column(:id, :id)
+        t.add_column(:description)
+        t.add_column(:place, :contract_place)
+        t.add_column(:responsible, :owner_name)
+      end
+      r.add_field "USER_POSITION", current_user.position
+      r.add_field "USER_NAME", current_user.displayname
+    end
+    send_data report.generate, type: 'application/msword',
+      filename: "documents.odt",
+      disposition: 'inline'
+  end
 
     def approval_sheet_odt    # Лист согласования
       report = ODFReport::Report.new("reports/approval-sheet-contract.odt") do |r|
