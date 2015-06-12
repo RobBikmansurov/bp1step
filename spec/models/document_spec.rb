10a7664b53b118922e723c1f2d4ad72e0cf9cb58
Rob Bikmansurov <robb@mail.ru>
2015-06-12 17:07:48 +0500

    upd: rspec tests

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,31 +1,12 @@
 require 'spec_helper'
 
 describe Document do
+
   context 'validations' do
-    it { should ensure_length_of(:name).is_at_least(10).is_at_most(200) }
-    it { should validate_numericality_of(:dlevel) }
-    #it { should ensure_inclusion_of(:dlevel).in_range(1..4) }
-    #it { should validate_presence_of(:bproce_id) }
     it { should validate_presence_of(:place) }
-    #it { should validate_presence_of(:part) }
     it { should validate_presence_of(:owner_id) }
-
-
-   it "it not valid if dlevel < 1 or > 4" do #validates :dlevel, :numericality => {:less_than => 5, :greater_than => 0}
-     @doc = FactoryGirl.create :document
-     @doc.dlevel = nil
-     expect(@doc).not_to be_valid
-     @doc.dlevel = 0
-     expect(@doc).not_to be_valid
-     @doc.dlevel = 1
-     expect(@doc).to be_valid
-     @doc.dlevel = 4
-     expect(@doc).to be_valid
-     @doc.dlevel = 5
-     expect(@doc).not_to be_valid
-   end
-
-
+    it { should validate_length_of(:name).is_at_least(10).is_at_most(200) }
+    it { should validate_numericality_of(:dlevel) }
   end
  
   context "associations" do
