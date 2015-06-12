917c4e8603556b1fa1fed117377f33fbac2a7cc2
Rob Bikmansurov <robb@mail.ru>
2015-06-12 17:07:21 +0500

    upd: rspec tests

--- spec/models/iresource_spec.rb
+++ spec/models/iresource_spec.rb
@@ -1,34 +1,19 @@
 require 'spec_helper'
 
 describe Iresource do
-  before(:each) do
-    @iresource = create(:iresource)
-  end
 
   context "validates" do
-    it "is valid with valid attributes: name, delevel, bproce_id" do
-      expect(@iresource).to be_valid
-    end
-
-  	#validates :label, :uniqueness => true, :length => {:minimum => 3, :maximum => 20}
-	it "is not valid if length of location < 3 or > 255" do   #validates :location, :length => {:minimum => 3, :maximum => 255}
-      @iresource.location = "12"
-      expect(@iresource).not_to be_valid
-      @iresource.location = "123"
-      expect(@iresource).to be_valid
-      @iresource.location = "1234567890" * 25 + "123456"
-      expect(@iresource).not_to be_valid
-      @iresource.location = "1234567890" * 25 + "12345"
-      expect(@iresource).to be_valid
-    end
+    it { should validate_presence_of(:label) }
+    it { should validate_uniqueness_of(:label) }
+    it { should validate_length_of(:label).is_at_least(3).is_at_most(20) }
+    it { should validate_presence_of(:location) }
+    it { should validate_length_of(:location).is_at_least(3).is_at_most(255) }
   end
 
   context "associations" do
-	it "belong_to :user" do
-      should belong_to(:user) # у ресурса может быть владелец
-    end
-  	#has_many :bproce_iresources
-  	#has_many :bproces, :through => :bproce_iresources
+    it { should belong_to(:user) }
+    it { should have_many(:bproce_iresources) }
+    it { should have_many(:bproces).through(:bproce_iresources) }
   end
 
 end
