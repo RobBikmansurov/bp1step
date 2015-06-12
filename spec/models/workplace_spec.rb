b1319746220a26be51b28e3942a02d6afcfc990a
Rob Bikmansurov <robb@mail.ru>
2015-06-12 17:05:13 +0500

    upd: rspec tests

--- spec/models/workplace_spec.rb
+++ spec/models/workplace_spec.rb
@@ -1,30 +1,18 @@
 require 'spec_helper'
 
 describe Workplace do
-  before(:each) do
-    @wp = create(:workplace)
-  end
 
   context "validates" do
     it { should validate_presence_of(:designation) }
     it { should validate_uniqueness_of(:designation) }
     it { should ensure_length_of(:designation).is_at_least(5).is_at_most(50) }
-    #it { should validate_presence_of(:description) }
   end
 
   context "associations" do
-    it "has_many :bproce_workplaces" do
-      should have_many(:bproce_workplaces) #рабочее место относится ко многим процессам
-    end
-    it "has_many :bproces, :through => :bproce_workplaces" do
-      should have_many(:bproces).through(:bproce_workplaces)
-    end
-    it "has_many :user_workplace" do
-      should have_many(:user_workplace) # на рабочем месте может быть много исполнителей
-    end
-    it "has_many :user_workplace, :through => :user_workplace" do
-      should have_many(:users).through(:user_workplace)
-    end
+    it { should have_many(:bproce_workplaces) }
+    it { should have_many(:bproces).through(:bproce_workplaces) }
+    it { should have_many(:user_workplace) }
+    it { should have_many(:users).through(:user_workplace) }
   end
 
 end
