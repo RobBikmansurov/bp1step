8fa818af35264b796ac748f66a0b4cc39687e48a
Rob Bikmansurov <robb@mail.ru>
2015-06-12 17:08:14 +0500

    upd: rspec tests

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Agent do
   context "validates" do
     it { should validate_presence_of(:name) }
-    it { should ensure_length_of(:name).is_at_least(3).is_at_most(255) }
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(255) }
   end
 
   context "associations" do
