9514e25f77ab45843c7f1bb141c62216c23b7463
Rob Bikmansurov <robb@mail.ru>
2015-07-01 16:50:24 +0500

    add: тесты маршрутов писем и требований

---
+++ spec/routing/letters_routing_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe LettersController, :type => :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/letters").to route_to("letters#index")
+    end
+
+    it "routes to #new" do
+      expect(:get => "/letters/new").to route_to("letters#new")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/letters/1").to route_to("letters#show", :id => "1")
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/letters/1/edit").to route_to("letters#edit", :id => "1")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/letters").to route_to("letters#create")
+    end
+
+    it "routes to #update" do
+      expect(:put => "/letters/1").to route_to("letters#update", :id => "1")
+    end
+
+    it "routes to #destroy" do
+      expect(:delete => "/letters/1").to route_to("letters#destroy", :id => "1")
+    end
+
+  end
+end
---
+++ spec/routing/requirements_routing_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe RequirementsController, :type => :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/requirements").to route_to("requirements#index")
+    end
+
+    it "routes to #new" do
+      expect(:get => "/requirements/new").to route_to("requirements#new")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/requirements/1").to route_to("requirements#show", :id => "1")
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/requirements/1/edit").to route_to("requirements#edit", :id => "1")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/requirements").to route_to("requirements#create")
+    end
+
+    it "routes to #update" do
+      expect(:put => "/requirements/1").to route_to("requirements#update", :id => "1")
+    end
+
+    it "routes to #destroy" do
+      expect(:delete => "/requirements/1").to route_to("requirements#destroy", :id => "1")
+    end
+
+  end
+end
