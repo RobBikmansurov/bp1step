e26f5aefeaddf9453f0c0eeabb4cbc18b4edd248
Rob Bikmansurov <robb@mail.ru>
2015-07-17 23:10:38 +0500

    fix: порядок исполнителей - сначала ответственный, затем остальные

--- app/models/user_letter.rb
+++ app/models/user_letter.rb
@@ -2,6 +2,8 @@ class UserLetter < ActiveRecord::Base
   include PublicActivity::Model
   tracked owner: Proc.new { |controller, model| controller.current_user }
 
+  default_scope { order('status DESC') }
+
   validates :letter, :presence => true
   validates :user, :presence => true
 
