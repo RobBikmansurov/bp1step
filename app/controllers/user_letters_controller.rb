44b1fb65f8e29a458d2dd584b4438156ae9e74e2
Rob Bikmansurov <robb@mail.ru>
2015-07-21 12:28:58 +0500

    upd: добавлена бизнес-логика: если нет ответственных - статус = Новое, если есть и статус Новое, то статус = Назначено

--- app/controllers/user_letters_controller.rb
+++ app/controllers/user_letters_controller.rb
@@ -13,14 +13,20 @@ class UserLettersController < ApplicationController
 
   def create
     @user_letter = UserLetter.create(params[:user_letter])
-    flash[:notice] = "Successfully created user_letter." if @user_letter.save
+    if @user_letter.save
+      flash[:notice] = "Successfully created user_letter."
+      letter = Letter.find(@user_letter.letter)
+      letter.update_column(:status, 5) if letter.status < 1 # если есть ответственные - статус = Назначено
+    end
     respond_with(@user_letter)
   end
 
   def destroy
-    @user_letter = UserLetter.find(params[:id])   # нашли удаляемую связь
-    @letter = Letter.find(@user_letter.letter_id) # запомнили письмо для этой удаляемой связи
-    @user_letter.destroy   # удалили связь
+    user_letter = UserLetter.find(params[:id])   # нашли удаляемую связь
+    @letter = Letter.find(user_letter.letter_id) # запомнили письмо для этой удаляемой связи
+    if user_letter.destroy   # удалили связь
+      @letter.update_column(:status, 0) if !@letter.user_letter.first # если нет ответственных - статус = Новое
+    end
     respond_with(@letter)  # вернулись в письмо
   end
 
