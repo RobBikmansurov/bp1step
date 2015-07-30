f50f16af49cde3ae9aae04a31067fa6daac594a5
Rob Bikmansurov <robb@mail.ru>
2015-07-30 11:04:08 +0500

    fix:

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,3 +5,5 @@
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register "text/msword", :odt
 #Mime::Type.register "text/pdf", :pdf
+#Mime::Type.register "applications/vnd.pdf", :pdf
+Mime::Type.register "applications/pdf", :pdf
