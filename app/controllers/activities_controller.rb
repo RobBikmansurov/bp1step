67a9f26ad20ff7d5599f6326fa55963b91b37848
Rob Bikmansurov <robb@mail.ru>
2015-08-06 12:33:57 +0500

    add: добавлено кол-во писем в разных статусах в фильтре писем, добавлено отображение протокола всех действий над письмом - добавлены действия по ответственным и файлам приложений

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,12 +1,24 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities_header = ''
     if params[:type].present?
-      @activities = PublicActivity::Activity.where(trackable_type: "#{params[:type]}").paginate(:per_page => 100, :page => params[:page])
+      @activities = PublicActivity::Activity.where(trackable_type: "#{params[:type]}")
       if params[:id].present?
-        @activities = PublicActivity::Activity.where(trackable_type: "#{params[:type]}", trackable_id: "#{params[:id]}").paginate(:per_page => 100, :page => params[:page])
+        case params[:type].downcase
+        when 'letter'
+          letter = Letter.find("#{params[:id]}")
+          @activities_header = "Письмо #{letter.name}"
+          ids = PublicActivity::Activity.select(:id).where(trackable_type: "#{params[:type]}", trackable_id: "#{params[:id]}") \
+            | PublicActivity::Activity.select(:id).where(trackable_type: "UserLetter", trackable_id: letter.user_letter.ids) \
+            | PublicActivity::Activity.select(:id).where(trackable_type: "LetterAppendix", trackable_id: letter.letter_appendix.ids)
+          @activities = PublicActivity::Activity.where(id: ids).order("created_at desc")
+        else
+          @activities = PublicActivity::Activity.where(trackable_type: "#{params[:type]}", trackable_id: "#{params[:id]}")
+        end
       end
     else
-      @activities = PublicActivity::Activity.order("created_at desc").paginate(:per_page => 100, :page => params[:page])
+      @activities = PublicActivity::Activity.order("created_at desc")
     end
+    @activities = @activities.paginate(:per_page => 100, :page => params[:page])
   end
 end
