dc3307473ccd3ab0a6b5bedeedf2b4177d3499eb
Rob Bikmansurov <robb@mail.ru>
2015-09-02 23:04:43 +0500

    add: test model UserLetter

--- spec/models/user_letter_spec.rb
+++ spec/models/user_letter_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe UserLetter, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserLetter do
+  context "validates" do
+    it { should validate_presence_of(:letter) }
+    it { should validate_presence_of(:user) }
+  end
+
+  context "associations" do
+    it { should belong_to(:letter) }
+    it { should belong_to(:user) }
+  end
 end
