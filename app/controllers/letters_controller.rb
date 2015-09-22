4b9ab6216ef374201395608a18a6a8cb92b15d96
Rob Bikmansurov <robb@mail.ru>
2015-09-22 16:07:12 +0500

    test:

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -171,6 +171,7 @@ class LettersController < ApplicationController
   def senders
       @senders = Letter.select(:sender).group(:sender, :status).order(:sender, :status).count
       #@senders = @senders.order(sort_column + ' ' + sort_direction).paginate(:per_page => 10, :page => params[:page])
+      @title_senders = 'Корреспонденты'
   end
 
   private
