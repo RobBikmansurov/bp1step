ec6e7d53de8b453b8978fed9f7609d4415592b5d
Rob Bikmansurov <robb@mail.ru>
2015-10-01 00:05:17 +0500

    red features

--- spec/features/agents_spec.rb
+++ spec/features/agents_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 RSpec.feature 'Managing agents', type: :feature do
 
-  scenario 'Finding the list of agents' do
-    visit '/'
+  context 'Finding the list of agents' do
+    describe 'any users' do
+      it "don't see Agents" do
+        visit '/'
 
-    click_link 'Контрагенты'
-    #expect(current_path).to eq('/about')
-
-    expect(current_path).to be('/agents')
-#    expect(page).to have_content("Войти")
+        expect(page).not_to have_content('/agents')
+        expect(page).to have_content("Войти")
+      end
+    end
+    
+    describe 'logged users' do
+      it "can see Agents" do
+        user = User.last
+        login user.email, user.password
+        visit '/agents'
+        expect(page).to have_content('/agents')
+        expect(page).to have_content("Войти")
+      end
+    end
   end
 end
