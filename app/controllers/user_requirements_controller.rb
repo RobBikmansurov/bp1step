1612db8baf1ad85c848ae668c30e09fc9a9eba3f
Rob Bikmansurov <robb@mail.ru>
2015-10-29 17:47:26 +0500

    upd: добавление исполнителей требования через autocomplete, оповещение о назначении/удалении исполнителей Требования

--- app/controllers/user_requirements_controller.rb
+++ app/controllers/user_requirements_controller.rb
@@ -13,14 +13,32 @@ class UserRequirementsController < ApplicationController
 
   def create
     @user_requirement = UserRequirement.new(params[:user_requirement])
-    flash[:notice] = "Successfully created user_requirement." if @user_requirement.save
+    if @user_requirement.save
+      flash[:notice] = "Successfully created user_requirement."
+      begin
+        UserRequirementMailer.user_requirement_create(@user_requirement, current_user).deliver_now    # оповестим нового исполнителя
+      rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
+        flash[:alert] = "Error sending mail to #{@user_requirement.user.email}"
+      end
+      requirement = Requirement.find(@user_requirement.requirement_id)
+      requirement.update_column(:status, 5) if requirement.status < 1 # если есть ответственные - статус = Назначено
+    else
+      flash[:alert] = "Error create user_requirement"
+    end
     respond_with(@user_requirement)
   end
 
   def destroy
     @user_requirement = UserRequirement.find(params[:id])   # нашли удаляемую связь
     @requirement = Requirement.find(@user_requirement.requirement_id) # запомнили требование для этой удаляемой связи
-    @user_requirement.destroy   # удалили связь
+    begin
+      UserRequirementMailer.user_requirement_destroy(@user_requirement, current_user).deliver_now    # оповестим исполнителя
+    rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
+      flash[:alert] = "Error sending mail to #{@user_requirement.user.email}"
+    end
+    if @user_requirement.destroy   # удалили связь
+      @requirement.update_column(:status, 0) if !@requirement.user_requirement.first # если нет ответственных - статус = Новое
+    end
     respond_with(@requirement)  # вернулись в требование
   end
 
