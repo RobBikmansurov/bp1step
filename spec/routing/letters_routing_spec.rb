ba264c885fe30060e33d5d9a460a4037efad8c49
Rob Bikmansurov <robb@mail.ru>
2015-10-30 13:36:42 +0500

    add: добавляет тесты роутинга

--- spec/routing/letters_routing_spec.rb
+++ spec/routing/letters_routing_spec.rb
@@ -29,16 +29,40 @@ RSpec.describe LettersController, :type => :routing do
       expect(:delete => "/letters/1").to route_to("letters#destroy", :id => "1")
     end
 
-    it "routes to #register" do
-      expect(:get => "/letters/1/register").to route_to("letters#register", :id => "1")
+    it "routes to #senders" do
+      expect(:get => "/letters/senders").to route_to("letters#senders")
     end
-
     it "routes to #log_week" do
       expect(:get => "/letters/log_week").to route_to("letters#log_week")
     end
-
     it "routes to #check" do
       expect(:get => "/letters/check").to route_to("letters#check")
+    end
+    it "routes to #register" do
+      expect(:get => "/letters/1/register").to route_to("letters#register", :id => "1")
+    end
+    it "routes to #appendix_create" do
+      expect(:get => "/letters/1/appendix_create").to route_to("letters#appendix_create", :id => "1")
+    end
+    it "routes to #appendix_delete" do
+      expect(:get => "/letters/1/appendix_delete").to route_to("letters#appendix_delete", :id => "1")
+    end
+    #  post :appendix_update
+    it "routes to #clone" do
+      expect(:get => "/letters/1/clone").to route_to("letters#clone", :id => "1")
+    end
+    it "routes to #create_outgoing" do
+      expect(:get => "/letters/1/create_outgoing").to route_to("letters#create_outgoing", :id => "1")
+    end
+    it "routes to #create_requirement" do
+      expect(:get => "/letters/1/create_requirement").to route_to("letters#create_requirement", :id => "1")
+    end
+    it "routes to #create_user" do
+      expect(:get => "/letters/1/create_user").to route_to("letters#create_user", :id => "1")
+    end
+     # post :update_user
+    it "routes to #register" do
+      expect(:get => "/letters/1/register").to route_to("letters#register", :id => "1")
     end
 
   end
