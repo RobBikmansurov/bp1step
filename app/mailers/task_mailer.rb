7526c1ed0cd952b4e9c608006875b1077f51dba7
Rob Bikmansurov <robb@mail.ru>
2015-11-10 09:16:23 +0500

    fix: отображает тему оповещений о задачах - выводит #ID в теме и теле письма

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -4,14 +4,14 @@ class TaskMailer < ActionMailer::Base
 
   def check_overdue_tasks(task, emails)   # рассылка исполнителям о просроченных письмах
     @task = task
-    mail(:to => emails, :subject => "BP1Step: не исполнена Задача #{@task.name}")
+    mail(:to => emails, :subject => "BP1Step: не исполнена Задача #{@task.id}")
   end
 
   def soon_deadline_tasks(task, emails, days, users)   # рассылка исполнителям о наступлении срока исполнения письма
     @task = task
     @users = users
     @days = days.to_i
-    mail(:to => emails, :subject => "BP1Step: #{@days} дн. на Задачу #{@task.name}")
+    mail(:to => emails, :subject => "BP1Step: #{@days} дн. на Задачу #{@task.id}")
   end
 
 end
