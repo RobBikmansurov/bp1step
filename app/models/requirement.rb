fb039b94e7a73918084bc573b3e9768fa2b2de20
Rob Bikmansurov <robb@mail.ru>
2016-03-22 12:46:03 +0500

    add: добавляет сортировку задач при отображении требования

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -10,6 +10,7 @@ class Requirement < ActiveRecord::Base
   belongs_to :author, :class_name => 'User'
   has_many :user, through: :user_requirement
   has_many :user_requirement, dependent: :destroy # ответственные за Требование
+  has_many :task
 
   attr_accessible :label, :date, :source, :duedate, :body, :status, :status_name, :result, :author_name, :letter_id
 
--- spec/models/requirement_spec.rb
+++ spec/models/requirement_spec.rb
@@ -1,10 +1,10 @@
-require 'spec_helper'
+#require 'spec_helper'
 
 describe Requirement do
 
   context "validates" do
-    it { should validate_presence_of(:label) }
-    it { should validate_length_of(:label).is_at_least(3).is_at_most(255) }
+    it { is_expected.to validate_presence_of(:label) }
+    it { is_expected.to validate_length_of(:label).is_at_least(3).is_at_most(255) }
   end
 
   context "associations" do
@@ -12,6 +12,6 @@ describe Requirement do
     it { should belong_to(:author) }
     it { should have_many(:user) }
     it { should have_many(:user_requirement).dependent(:destroy) }
+    it { should have_many(:task) }
   end
-
 end
