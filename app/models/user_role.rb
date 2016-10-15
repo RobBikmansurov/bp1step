0dcfa9208cdbab7a24954c0a36860f6dbe5a61cb
robb <robb@mail.ru>
2016-10-15 18:55:54 +0500

    small fix

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,10 +1,14 @@
 class UserRole < ActiveRecord::Base
-  include PublicActivity::Model
-  tracked owner: Proc.new { |controller, model| controller.current_user }
+  include PublicActivity::Common
+  #include PublicActivity::Model
+  #tracked owner: Proc.new { |controller, model| controller.current_user }
 
   validates :role, :presence => true
   validates :user, :presence => true
 
   belongs_to :role
   belongs_to :user
+
+  attr_accessible :user_id, :role_id
 end
+
