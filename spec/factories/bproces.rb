c348370c2b77834d2eb8ccad2aaa554dc673ebc9
robb <robb@mail.ru>
2016-10-15 18:44:35 +0500

    small changes

--- spec/factories/bproces.rb
+++ spec/factories/bproces.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :bproce do
     sequence(:id) { |n| "#{n}" }
-    sequence(:name) { |n| "bproce_name_#{id}" }
+    name { "bproce_name_#{id}" }
     shortname { "bp.#{id}" }
     fullname 'test process full name'
   end
