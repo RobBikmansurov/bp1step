41a096aea8e03f28d5f804bc5275f362fecbbd89
robb <robb@mail.ru>
2016-11-26 00:29:52 +0500

    fixed

--- spec/factories/requirements.rb
+++ spec/factories/requirements.rb
@@ -1,14 +1,13 @@
 FactoryGirl.define do
   factory :requirement do
-    label "MyString"
+    sequence(:id) { |n| "#{n}" }
+    label         { "requirement#{id}" }
     date Date.current
     duedate Date.current
     source "MyString"
     body "MyText"
     status "MyString"
     result "MyText"
-    letter nil
-    user nil
   end
 
 end
