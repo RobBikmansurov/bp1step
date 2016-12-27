d5eb74736498dd11e780da26d6d624bdd3b9a482
Rob Bikmansurov <robb@mail.ru>
2016-12-27 16:32:58 +0500

    agent validation max length

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,13 +1,15 @@
 # Agent
 class Agent < ActiveRecord::Base
   attr_accessible :shortname, :name, :town, :address, :contacts, :agent_name, :note
-  
+
   has_many :contract, through: :agent_contract
   has_many :agent_contract, dependent: :destroy
 
   validates :name, presence: true,
                    length: { minimum: 3, maximum: 255 }
   validates :town, length: { maximum: 30 }
+  validates :shortname, length: { maximum:255 }
+  validates :address, length: { maximum:255 }
 
   def self.search(search)
     if search
