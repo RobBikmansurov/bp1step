5bbdb4b15cb9b76fd5f2467a6b0ff361469a353d
robb <robb@mail.ru>
2017-04-05 20:45:58 +0500

    try features spec

---
+++ spec/support/feature/session_helpers.rb
@@ -0,0 +1,10 @@
+module Features
+  module SessionHelpers
+    def sign_in_with(email, password)
+      visit "/users/sign_in"
+      fill_in 'user_email', with: email
+      fill_in 'user_password', with: password
+      click_button 'Sign in'
+    end
+  end
+end
