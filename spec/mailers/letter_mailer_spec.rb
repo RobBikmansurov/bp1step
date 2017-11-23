1d028b1d4cb0e868e9f1396fbb44f07afa0e593a
Rob Bikmansurov <robb@mail.ru>
2017-11-23 10:13:16 +0500

    fixed spec and code

--- spec/mailers/letter_mailer_spec.rb
+++ spec/mailers/letter_mailer_spec.rb
@@ -6,8 +6,9 @@ require 'rails_helper'
 # UserLetterMailer.user_letter_create(user_letter, current_user).deliver_now    # оповестим нового исполнителя
 
 describe LetterMailer do
-  let(:letter) { FactoryBot.create(:letter) }
-  let(:user) { FactoryBot.create(:user) }
+  let!(:letter) { FactoryBot.create(:letter) }
+  let!(:user) { FactoryBot.create(:user) }
+  let(:user_letter) { FactoryBot.create(:user_letter, user_id: user.id, letter_id: letter.id) }
 
   describe 'check_overdue_letters' do
     # check_overdue_letters(letter, emails) # рассылка исполнителям о просроченных письмах
@@ -47,15 +48,18 @@ describe LetterMailer do
 
   describe 'update_letter' do
     # update_letter(letter, current_user, result) # рассылка об изменении письма
-    let(:current_user) { FactoryBot.create(:user, displayname: 'user') }
-    let(:mail) { LetterMailer.update_letter(letter, [current_user.email], 'updated') }
+    let(:current_user) { FactoryBot.create(:user) }
+    let(:mail) { LetterMailer.update_letter(letter, current_user, 'updated') }
 
     it 'renders the subject' do
-      expect(mail.subject).to eql("BP1Step: 1 дн. на Письмо #{letter.name}")
+      expect(mail.subject).to eql("BP1Step: Письмо #{letter.name} [#{LETTER_STATUS.key(letter.status)}] изменено")
     end
 
     it 'renders the receiver email' do
-      expect(mail.to).to eql([user.email])
+      user1 = FactoryBot.create(:user)
+      user_letter = FactoryBot.create(:user_letter, user_id: current_user.id, letter_id: letter.id)
+      user_letter1 = FactoryBot.create(:user_letter, user_id: user1.id, letter_id: letter.id)
+      expect(mail.to).to eql([current_user.email, user1.email])
     end
 
     it 'renders the sender email' do
@@ -63,7 +67,7 @@ describe LetterMailer do
     end
 
     it 'assigns @text' do
-      expect(mail.body.encoded).to match('исполнить ВСЕ требования письма')
+      expect(mail.body.encoded).to match('в которое внесены изменения')
     end
   end
 end
