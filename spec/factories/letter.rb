58c0dc927d88e37ab95fdf2433bb3a01df632c80
Rob Bikmansurov <robb@mail.ru>
2018-01-11 11:31:34 +0500

    updated factories

---
+++ spec/factories/letter.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :letter do
+    sequence(:id, &:to_s)
+    number { "LET--#{id}" }
+    regnumber 'MyString'
+    regdate Date.current
+    date Date.current
+    duedate Date.current + 1
+    subject 'MyString'
+    source 'MyString'
+    sender 'MyText'
+    body 'MyText'
+    status 1
+    result 'MyText'
+  end
+end
