f707cb99fe5d9104ac35eb8227884981e8cde75e
Rob Bikmansurov <robb@mail.ru>
2018-02-07 11:22:19 +0500

    improve code and tests for user`s controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,68 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let!(:user) { FactoryBot.create(:user, office: 1) }
+  let!(:user1) { FactoryBot.create(:user, office: 2) }
+
   before(:each) do
-    @user = FactoryBot.create(:user)
-    @user.roles << Role.find_or_create_by(name: 'admin', description: 'description')
-    sign_in @user
+    user.roles << Role.find_or_create_by(name: 'admin', description: 'description')
+    sign_in user
     allow(controller).to receive(:authenticate_user!).and_return(true)
   end
 
-  describe 'GET show' do
-    it 'assigns @usr' do
-      get :show, id: @user.id
+  describe 'GET index' do
+    it 'assigns all users as @users' do
+      get :index, {}
       expect(response).to be_success
-      expect(response).to render_template(:show)
-      expect(assigns(:usr)).to eq(@user)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('users/index')
     end
 
-    it 'render users search collection' do
-      # xhr :get, :show, id: @user.id, "search" => @user.displayname
-      # expect(response).to be_success
-      # expect(assigns(:users)).not_to be_empty
-      # expect(response).to render_template(:index)
+    it 'lists all users' do
+      get :index
+      expect(assigns(:users)).to match_array([user, user1])
     end
-  end
-
-  describe 'GET uworkplaces' do
-    it 'should be success' do
-      xhr :get, :uworkplaces, { id: @user.id }, format: :html
-      expect(response).to render_template(:uworkplaces)
+    it 'lists users from office' do
+      get :index, params: { office: 1 }
+      expect(assigns(:users)).to match_array([user])
+      get :index, params: { office: 2 }
+      expect(assigns(:users)).to match_array([user1])
     end
-  end
-
-  describe 'GET uroles' do
-    it 'should be success' do
-      xhr :get, :uroles, { id: @user.id }, format: :html
-      expect(response).to render_template(:uroles)
-    end
-  end
-
-  describe 'GET documents' do
-    it 'should be success' do
-      xhr :get, :documents, { id: @user.id }, format: :html
-      expect(response).to render_template(:documents)
-    end
-  end
-
-  describe 'GET contracts' do
-    it 'should be success' do
-      xhr :get, :contracts, { id: @user.id }, format: :html
-      expect(response).to render_template(:contracts)
-    end
-  end
-
-  describe 'GET resources' do
-    it 'should be success' do
-      xhr :get, :resources, { id: @user.id }, format: :html
-      expect(response).to render_template(:resources)
-    end
-  end
-
-  describe 'GET processes' do
-    it 'should be success' do
-      xhr :get, :processes, { id: @user.id }, format: :html
-      expect(response).to render_template(:processes)
+    it 'lists users with role' do
+      get :index, params: { role: 'admin' }
+      expect(assigns(:users)).to match_array([user])
     end
   end
 end
