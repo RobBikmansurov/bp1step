251e6a71c24e878bb7cbcf68c3c1be71f596e3f1
Rob Bikmansurov <robb@mail.ru>
2018-04-17 10:09:41 +0500

    add bapp by name and add bproce by name to bproce_bapp fixed

--- app/controllers/bproce_bapps_controller.rb
+++ app/controllers/bproce_bapps_controller.rb
@@ -3,13 +3,13 @@
 class BproceBappsController < ApplicationController
   respond_to :html, :json
   helper_method :sort_column, :sort_direction
-  before_action :authenticate_user!, only: %i[edit new]
+  before_action :authenticate_user!, only: %i[edit create]
   before_action :bproce_bapp, except: %i[index create]
 
   def create
-    # bproce_bapp = logger.debug "params = #{params[:bproce_bapp_bproce_id]}"
-    @bproce = Bproce.find_by(name: params[:iresource_bproce_id])
     @bproce_bapp = BproceBapp.new(bproce_bapp_params)
+    add_bproce_by_name(@bproce_bapp)
+    add_bapp_by_name(@bproce_bapp)
     if @bproce_bapp.save
       flash[:notice] = 'Successfully created bproce_bapp.'
       redirect_to(@bproce_bapp.bapp)
@@ -47,14 +47,14 @@ class BproceBappsController < ApplicationController
   end
 
   def update
-    flash[:notice] = 'Successfully updated bproce_bapp.' if @bproce_bapp.update_attributes(bproce_bapp_params)
+    flash[:notice] = 'Successfully updated bproce_bapp.' if @bproce_bapp.update(bproce_bapp_params)
     respond_with(@bproce_bapp)
   end
 
   private
 
   def bproce_bapp_params
-    params.require(:bproce_bapp).permit(:bproce_id, :bapp_id, :apurpose)
+    params.require(:bproce_bapp).permit(:bproce_id, :bapp_id, :apurpose, :bapp_name, :bproce_name)
   end
 
   def sort_column
@@ -68,5 +68,18 @@ class BproceBappsController < ApplicationController
   def bproce_bapp
     @bproce = Bproce.find(params[:bproce_id]) if params[:bproce_id].present?
     @bproce_bapp = params[:id].present? ? BproceBapp.find(params[:id]) : BproceBapp.new(bproce_bapp_params)
+  end
+
+  # добавляем процесс по имени к приложению
+  def add_bproce_by_name(bproce_bapp)
+    return if bproce_bapp_params[:bproce_name].blank?
+    bproce = Bproce.find_by(name: bproce_bapp_params[:bproce_name])
+    bproce_bapp.bproce_id = bproce&.id
+  end
+
+  def add_bapp_by_name(_bproce_bapp)
+    return if bproce_bapp_params[:bapp_name].blank?
+    bapp = Bapp.find_by(name: bproce_bapp_params[:bapp_name])
+    @bproce_bapp.bapp_id = bapp&.id
   end
 end
