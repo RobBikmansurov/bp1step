253bc75a6c9259789c059392f9c98cfc761aa9bb
Rob Bikmansurov <robb@mail.ru>
2018-08-02 10:17:17 +0500

    improve models`s specs

--- spec/models/bapp_spec.rb
+++ spec/models/bapp_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe Bapp do
   context 'validates' do
     it { should validate_presence_of(:name) }
-    # it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name) }
     it { should validate_length_of(:name).is_at_least(4).is_at_most(50) }
     it { should validate_presence_of(:description) }
   end
--- spec/models/workplace_spec.rb
+++ spec/models/workplace_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe Workplace do
   context 'validates' do
     it { should validate_presence_of(:designation) }
-    # it { should validate_uniqueness_of(:designation) }
+    it { should validate_uniqueness_of(:designation) }
     it { should validate_length_of(:designation).is_at_least(5).is_at_most(50) }
   end
 
