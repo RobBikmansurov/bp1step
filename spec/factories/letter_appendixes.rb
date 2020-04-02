610cb2ff20b0fcb8eee34bcc9d55c8616982599f
Rob Bikmansurov <robb@mail.ru>
2020-04-02 13:14:35 +0500

    style: improve code style with rubocop

--- spec/factories/letter_appendixes.rb
+++ spec/factories/letter_appendixes.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :letter_appendix do
-    appendix { File.new(Rails.root.join('spec', 'support', 'test.odt')) }
+    appendix { File.new(Rails.root.join('spec/support/test.odt')) }
     name { 'MyString' }
     letter
   end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,12 +3,12 @@
 FactoryBot.define do
   factory :order do
     sequence(:id, &:to_s)
-    order_type "Распоряжение об открытии счетов"
-    codpred 1
-    author 1
-    contract_number "КОНТРАКТ++123"
+    order_type { 'Распоряжение об открытии счетов' }
+    codpred { 1 }
+    author { 1 }
+    contract_number { 'КОНТРАКТ++123' }
     contract_date { Date.current - 10 }
     due_date { Date.current + 1 }
-    status "Новое"
+    status { 'Новое' }
   end
 end
