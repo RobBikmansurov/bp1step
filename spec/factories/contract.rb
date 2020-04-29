dd9233a13936bce39d8b305066e2fc8cf1171e6d
Rob Bikmansurov <robb@mail.ru>
2020-04-29 23:06:07 +0500

    test: try to improve tests

--- spec/factories/contract.rb
+++ spec/factories/contract.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     sequence(:id, &:to_s)
     number        { "10#{id}" }
     name          { "name#{id}" }
-    contract_type { 'Contract' }
+    contract_type { 'Договор' }
     status        { 'Действует' }
-    date_begin    { Date.current }
+    date_begin    { Date.current - 20 }
     date_end      { Date.current + 100 }
     description   { 'contract_description' }
     trait :invalid do
