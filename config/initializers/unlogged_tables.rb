1025802d06be6213fc185ab117b66ccf88815816
Rob Bikmansurov <robb@mail.ru>
2020-08-12 12:32:28 +0500

    feat: test: add creation unplugged tables in test environment

---
+++ config/initializers/unlogged_tables.rb
@@ -0,0 +1,7 @@
+# config/initializers/unlogged_tables.rb
+
+if Rails.env.test?  
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.create_unlogged_tables = true 
+  end
+end
